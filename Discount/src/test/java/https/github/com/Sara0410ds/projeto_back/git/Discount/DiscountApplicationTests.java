c825d3bb7480fdbd7f3ce467fa160f0df7ac702c
Sara0410ds <saraduarte0410@gmail.com>
2025-11-04 18:57:46 -0300

    Primeiro Commit

---
+++ Discount/src/test/java/https/github/com/Sara0410ds/projeto_back/git/Discount/DiscountApplicationTests.java
@@ -0,0 +1,13 @@
+package https.github.com.Sara0410ds.projeto_back.git.Discount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
